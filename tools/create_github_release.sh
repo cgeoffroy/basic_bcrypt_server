deab3b1fcd6b030817f3569c54a58c247df8f977
cgeoffroy <cgeoffroy@users.noreply.github.com>
2016-01-22 15:07:31 +0100

    Debugging script for travis

--- tools/create_github_release.sh
+++ tools/create_github_release.sh
@@ -9,11 +9,17 @@ GH_REPO="$(echo ${TRAVIS_REPO_SLUG} | cut -d '/' -f 2)"
 
 NAME="v${VERSION}-${BRANCHNAME}.${PRERELEASEINFO}+b${BUILDNAME}"
 
-env
+echo "VERSION=$VERSION"
+echo "BRANCHNAME=$BRANCHNAME"
+echo "PRERELEASEINFO=$PRERELEASEINFO"
+echo "BUILDNAME=$BUILDNAME"
+echo "GH_USER=$GH_USER"
+echo "GH_REPO=$GH_REPO"
+echo "NAME=$NAME"
 
-#github-release release --user "${GH_USER}" --repo "${GH_REPO}" --tag "v${VERSION}" --name "${NAME}" --draft="true"
+#github-release release --user "${GH_USER}" --repo "${GH_REPO}" --tag "v${VERSION}" --name "${NAME}" --draft="true" --description "Built by Travis-ci.org"
 
-for F in $(find ${GOPATH}/bin/${GH_REPO}-xc/${VERSION}-* -type 'f' -name '*.tar.gz')
+for F in $(find ${GOPATH}/bin/${GH_REPO}-xc/${VERSION}-* -type 'f' -name '*.tar.gz'); do
     echo "F=${F}"
 done
 
