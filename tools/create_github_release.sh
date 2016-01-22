7522dd9a71d31fb3ab6ddcde28527ef2279da1ea
cgeoffroy <cgeoffroy@users.noreply.github.com>
2016-01-22 15:16:24 +0100

    Creating a github release tag and upload binaries

--- tools/create_github_release.sh
+++ tools/create_github_release.sh
@@ -6,21 +6,18 @@ PRERELEASEINFO="$(jq -r '.PrereleaseInfo' .goxc.json)"
 BUILDNAME="$(jq -r '.BuildName' .goxc.json)"
 GH_USER="$(echo ${TRAVIS_REPO_SLUG} | cut -d '/' -f 1)"
 GH_REPO="$(echo ${TRAVIS_REPO_SLUG} | cut -d '/' -f 2)"
-
 NAME="v${VERSION}-${BRANCHNAME}.${PRERELEASEINFO}+b${BUILDNAME}"
 
-echo "VERSION=$VERSION"
-echo "BRANCHNAME=$BRANCHNAME"
-echo "PRERELEASEINFO=$PRERELEASEINFO"
-echo "BUILDNAME=$BUILDNAME"
-echo "GH_USER=$GH_USER"
-echo "GH_REPO=$GH_REPO"
-echo "NAME=$NAME"
+echo "Creating a new draft release ${NAME}"
 
-#github-release release --user "${GH_USER}" --repo "${GH_REPO}" --tag "v${VERSION}" --name "${NAME}" --draft="true" --description "Built by Travis-ci.org"
+github-release release --user "${GH_USER}" --repo "${GH_REPO}" --tag "v${VERSION}" --name "${NAME}" --draft="true" --description "Built by Travis-ci.org"
 
 for F in $(find ${GOPATH}/bin/${GH_REPO}-xc/${VERSION}-* -type 'f' -name '*.tar.gz'); do
-    echo "F=${F}"
+    echo "Uploading ${F}"
+
+    github-release upload --user "${GH_USER}" --repo "${GH_REPO}" --tag "v${VERSION}" --name "${NAME}" -f ${F}
 done
 
-#github-release upload --user "${GH_USER}" --repo "${GH_REPO}" --tag "v${VERSION}" --name "${NAME}" -f
+echo "Removing the draft status on tag ${NAME}"
+
+github-release edit --user "${GH_USER}" --repo "${GH_REPO}" --tag "v${VERSION}" --name "${NAME}" --draft="false"
